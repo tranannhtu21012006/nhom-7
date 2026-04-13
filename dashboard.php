bc5e1d928384bbcfcef62e716844090b725ecd32
AT2711 <trananhtuanpct06@gmail.com>
2026-04-13 21:23:18 +0700

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,45 @@
+<div class="container mt-5">
+
+<div class="card shadow p-4 rounded-4">
+
+<h1 class="text-center mb-4">
+
+ADMIN DASHBOARD
+
+</h1>
+
+<div class="d-grid gap-3">
+
+<a href="index.php?action=listArticle"
+class="btn btn-primary btn-lg py-3">
+
+Quản lý bài viết
+
+</a>
+
+<a href="index.php?action=listCategory"
+class="btn btn-success btn-lg py-3">
+
+Quản lý danh mục
+
+</a>
+
+<a href="index.php?action=reported_comments"
+class="btn btn-warning btn-lg py-3">
+
+Quản lý bình luận
+
+</a>
+
+<a href="index.php?action=manage_users"
+class="btn btn-dark btn-lg py-3">
+
+Quản lý tài khoản
+
+</a>
+
+</div>
+
+</div>
+
+</div>
